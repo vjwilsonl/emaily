54c21186623ff57a5c9ff67b7c39cfd2df1ddcbb
vjwilsonl <vjwilsonl@gmail.com>
2018-04-19 16:57:31 +0800

    finished auth flow

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User'); // must define the schema first before using in passport.js file
+require('./services/passport.js');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'here' });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, //30 days
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-const PORT = process.env.PORT || 5000;
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 5000; //heroku
 app.listen(PORT);
