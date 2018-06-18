1f1eff323c30b591ba4bae77990ad592a7f11cc9
vjwilsonl <vjwilsonl@gmail.com>
2018-06-18 17:09:51 +0800

    Added biling and client side app!

---
+++ client/src/index.js
@@ -0,0 +1,23 @@
+// Redux configs
+import 'materialize-css/dist/css/materialize.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+
+import App from './components/App';
+import reducers from './reducers';
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+); //document refer to public/index.html
+
+console.log('STRIPE KEY IS', process.env.REACT_APP_STIPE_KEY);
+console.log(process.env);
+console.log('Environment is', process.env.NODE_ENV); //read documentation
