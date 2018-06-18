2baeaaf0ba352835438cf405b257d2bd930b1c41
vjwilsonl <vjwilsonl@gmail.com>
2018-06-18 18:39:17 +0800

    Test

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === 'production') {
   // if it does not recognize the route
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')); //leave to reactroute
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')); //leave to reactroutea
   });
 }
 
